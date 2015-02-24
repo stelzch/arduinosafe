9296f97ef2065ba07c7dba7f8dd072d0e8b1a072
Christoph Stelz <mail@ch-st.de>
2015-02-24 19:41:49 +0100

    Added enum with available states

--- src/Driver.h
+++ src/Driver.h
@@ -1,7 +1,13 @@
 class Driver {
 	public:
+		enum States {
+			MainMenu,
+			ViewPWs
+		};
 		UserInterface userInterface;
 		Database db;
+		void callState(
 	private:
 		State& currentState;
+
 };
