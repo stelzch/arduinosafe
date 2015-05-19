98e60f6bb1bdf2888f1a7b5e17a6e0b61c0af262
Valentin Pratz <vp@vocaword.org>
2015-05-19 20:00:50 +0200

    Adpated c driver for tft display.
    Input function's ouput variable needs work. Weird output when setting in loop

--- ArduinoDisplay.ino
+++ ArduinoDisplay.ino
@@ -1,44 +1,54 @@
-#include <Bounce2.h>
-#include <LiquidCrystal.h> //http://engineeringlearning.blogspot.de/2013/10/interfacing-lcd-without-potentiometer.html
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
+#include <SPI.h>
 
-#define rightBtnPin 7
-#define leftBtnPin 10
-#define okBtnPin 8
-#define backBtnPin 9
+// TFT display and SD card will share the hardware SPI interface.
+// Hardware SPI pins are specific to the Arduino board type and
+// cannot be remapped to alternate pins.  For Arduino Uno,
+// Duemilanove, etc., pin 11 = MOSI, pin 12 = MISO, pin 13 = SCK.
+#define SD_CS    4  // Chip select line for SD card
+#define TFT_CS  10  // Chip select line for TFT display
+#define TFT_DC   8  // Data/command line for TFT
+#define TFT_RST  -1  // Reset line for TFT (or connect to +5V)
+
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+
+#define BUTTON_NONE 0
+#define BUTTON_DOWN 1
+#define BUTTON_RIGHT 2
+#define BUTTON_SELECT 3
+#define BUTTON_UP 4
+#define BUTTON_LEFT 5
 
 char ch;
-int Contrast = 15;
 char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 char* mainMenu[] = { "Menue", "Passwort-Auswahl", "AFA", "MPasswort aendern", "Kiwi", "" };
 // inits
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
-
-Bounce rightBtn = Bounce();
-Bounce leftBtn = Bounce();
-Bounce okBtn = Bounce();
-Bounce backBtn = Bounce();
 
 void setup()
 {
-	analogWrite(6, Contrast);
-	lcd.begin(16, 2);
 	Serial.begin(9600);
 	digitalWrite(13, LOW);
-	delay(1000);
 	digitalWrite(13, HIGH);
+        // Initialize 1.8" TFT
+        tft.initR(INITR_BLACKTAB);   // initialize a ST7735S chip, black tab
+        Serial.println("OK!");
+        tft.fillScreen(ST7735_BLACK);
+        tft.setRotation(1);
+}
 
-	rightBtn.attach(rightBtnPin);
-	rightBtn.interval(5);
-
-	leftBtn.attach(leftBtnPin);
-	leftBtn.interval(5);
-
-	okBtn.attach(okBtnPin);
-	okBtn.interval(5);
-
-	backBtn.attach(backBtnPin);
-	backBtn.interval(5);
+uint8_t readButton(void) {
+  float a = analogRead(3);
+  
+  a *= 5.0;
+  a /= 1024.0;
+  
+  if (a < 0.2) return BUTTON_RIGHT;
+  if (a < 1.0) return BUTTON_DOWN;
+  if (a < 1.5) return BUTTON_SELECT;
+  if (a < 2.0) return BUTTON_LEFT;
+  if (a < 3.2) return BUTTON_UP;
+  else return BUTTON_NONE;
 }
 
 void loop()
@@ -66,51 +76,42 @@ int sizeArray(char* array[])
 
 void writeLine1(char content[])
 {
-	lcd.setCursor(0, 0);
-	lcd.print("                ");
-	lcd.setCursor(0, 0);
-	lcd.print(content);
+	tft.setCursor(0, 0);
+	tft.print("                ");
+	tft.setCursor(0, 0);
+	tft.print(content);
 }
 
 void writeLine1(char content[], int pos)
 {
-	lcd.setCursor(pos, 0);
-	lcd.print(content);
+	tft.setCursor(pos, 0);
+	tft.print(content);
 }
 
 void writeLine2(char content[])
 {
-	lcd.setCursor(0, 1);
-	lcd.print("                ");
-	lcd.setCursor(0, 1);
-	lcd.print(content);
+	tft.setCursor(0, 15);
+	tft.print("                ");
+	tft.setCursor(0, 15);
+	tft.print(content);
 }
 
 void writeLine2(char content[], int pos)
 {
-	lcd.setCursor(pos, 1);
-	lcd.print(content);
+	tft.setCursor(pos * 8, 15);
+	tft.print(content);
 }
 
 //---------------------------------------------------------------
 
 
-
-
-boolean xSecSwitch(Bounce btn, int howLong)
-{
-	unsigned long timer = millis();
-	while (btn.read());
-	timer = millis() - timer;
-
-	return(timer >= howLong * 1000);
-}
-
 char* input(char* headline, char* alphabet, int inputLength)  //inputLength > 7
 {
+        tft.fillScreen(ST7735_BLACK);
 	char buffer[inputLength];
 	char scroll[8] = "";
-	char output[8] = "";
+	char output[8] = "       ";
+        int letter_pos = 0;
 	int pos = 0;
 	int i = 0;
 	int p; //Hilfsvar
@@ -119,7 +120,10 @@ char* input(char* headline, char* alphabet, int inputLength)  //inputLength > 7
 
 	Serial.println("laenge Alpha. :" + alphabetLength);
 
-	writeLine1(headline);
+	
+        writeLine1(headline);
+        writeLine2(scroll, 9);
+	writeLine2("||", 7);
 
 	while (i < inputLength)
 	{
@@ -190,24 +194,25 @@ char* input(char* headline, char* alphabet, int inputLength)  //inputLength > 7
 
 
 		scroll[7] = '\0';
-		writeLine2(scroll, 9);
-		writeLine2("||", 7);
 
 		//   Serial.println(scroll + '|' + pos);
 
 		//--------------
-
-		while (rightBtn.update() == 0 && leftBtn.update() == 0 && okBtn.update() == 0 && backBtn.update() == 0);
-
-		if (rightBtn.read() == 1)
-		{
+                
+                uint8_t b;
+                b = readButton();
+                if (b == BUTTON_DOWN) {
 			pos++;
-		}
-
-		if (leftBtn.read() == 1)
-		{
+                        delay(500);
+                }
+                if (b == BUTTON_LEFT) {
+                }
+                if (b == BUTTON_UP) {
 			pos--;
-		}
+                        delay(500);
+                }
+                if (b == BUTTON_RIGHT) {
+                }
 
 		if (pos < 0)
 		{
@@ -217,6 +222,19 @@ char* input(char* headline, char* alphabet, int inputLength)  //inputLength > 7
 		{
 			pos = pos - alphabetLength;
 		}
+                if (b == BUTTON_SELECT) {
+                        buffer[letter_pos] = alphabet[pos];
+                        letter_pos++;
+                }                
+                if (b != BUTTON_NONE) {
+                   tft.fillScreen(ST7735_BLACK);
+                   writeLine1(headline);
+                   writeLine2(scroll, 9);
+		   writeLine2("||", 7);
+                   writeLine2(output);
+                   Serial.println(output);
+                   delay(200);
+                }
 		//--------------
 
 	}
@@ -226,41 +244,48 @@ char* input(char* headline, char* alphabet, int inputLength)  //inputLength > 7
 
 int showMenu(char* entries[])
 {
+        tft.fillScreen(ST7735_BLACK);
 	writeLine1(entries[0]);
 	boolean endLoop = false;
 	int rueckgabe;
 	int i = 1;
+        writeLine2(entries[i]);
 
 	while (endLoop == false)
 	{
-		writeLine2(entries[i]);
+                uint8_t b;
+		b = readButton();
 
-		while (rightBtn.update() == 0 && leftBtn.update() == 0 && okBtn.update() == 0 && backBtn.update() == 0);
-
-		if (rightBtn.read() == 1)
-		{
+                if (b == BUTTON_DOWN) {
 			i++;
 			if (*(entries[i]) == 0)
 			{
 				i = 1;
 			}
-		}
-
-		if (leftBtn.read() == 1)
-		{
+                }
+                if (b == BUTTON_LEFT) {
+                }
+                if (b == BUTTON_UP) {
 			i--;
 			if (i == 0)
 			{
 				i = sizeArray(entries) - 1;
 			}
-		}
+                }
+                if (b == BUTTON_RIGHT) {
+                }
 
-		if (okBtn.read() == 1)
+		if (b == BUTTON_SELECT)
 		{
 			//writeLine1(entries[i]);
 			endLoop = true;
 		}
+                if (b != BUTTON_NONE) {
+                  tft.fillScreen(ST7735_BLACK);
+                  writeLine1(entries[0]);
+                  writeLine2(entries[i]);
+                  delay(500);
+                }
 	}
-	lcd.clear();
 	return i;
 }
