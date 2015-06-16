f0c00fab96f2bc7d3ac16074810a4f6b51b4e7f4
Christoph Stelz <mail@ch-st.de>
2015-06-16 18:10:54 +0200

    tools.h with various converting functions and a C++-AESLibrary added

---
+++ lib/tools.h
@@ -0,0 +1,19 @@
+#ifndef TOOLS_H_
+#define TOOLS_H_
+/**
+ * Converts an array of chars to an array of bytes
+ * @param chararray the array which will be converted to a byte array
+ * @return the bytearray
+ */
+byte* chararray_to_bytearray(char* chararray) {
+  return reinterpret_cast<byte*>(chararray);
+};
+/**
+ * Convert an array of bytes to an array of chars
+ * @param bytearray the array which will be converted to a char array
+ * @return the chararray
+ */
+char* bytearray_to_chararray(byte* bytearray) {
+  return reinterpret_cast<char*>(bytearray);
+}
+#endif
